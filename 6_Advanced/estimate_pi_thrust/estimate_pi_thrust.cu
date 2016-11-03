6340a7bbbbb497a95f7df9e0ffadcd45f5ec3304
Alex Volkov <avolkov@nvidia.com>
2016-11-02 22:40:42 -0700

    Fix openmp implementation.
    The rand function was not re-entrant and slowed down omp. Fixed by using
    rand_r.

--- 6_Advanced/estimate_pi_thrust/estimate_pi_thrust.cu
+++ 6_Advanced/estimate_pi_thrust/estimate_pi_thrust.cu
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <sstream>
 #include <cmath>
-#include <sys/time.h>
 
-// #include <thread>
+//#include <chrono>
+//#include <thread>
+
+#include <sys/time.h>
 
 #include <omp.h>
 
@@ -20,8 +22,41 @@
 //#define UNUSED(x) (void)x;
 #define UNUSED(x) [&x]{}()
 
-
 typedef long long unsigned int UINT64;
+
+
+// Generate pi trial.
+//struct Genpit {
+//private:
+//    thrust::default_random_engine rng_;
+//    thrust::uniform_real_distribution<float> dist_;
+//
+//public:
+//    Genpit() {
+//        thrust::default_random_engine rng(clock());
+//        thrust::uniform_real_distribution<float> dist(0.0f, 1.0f);
+//        rng_ = rng;
+//        dist_ = dist;
+//    }
+//
+//    __host__ __device__ __forceinline__ float operator()(UINT64 id_or_seed)
+//    {
+//        float x, y;
+//        #ifdef __CUDA_ARCH__ // macro true on device and false on host.
+//            // thrust::default_random_engine rng(clock());
+//            // thrust::uniform_real_distribution<float> dist(0.0f, 1.0f);
+//            rng_.discard(id_or_seed);
+//            x = dist_(rng_);
+//            y = dist_(rng_);
+//        #else
+//            unsigned int rseed_ = id_or_seed;
+//            x = (float)rand_r(&rseed_)/RAND_MAX;
+//            y = (float)rand_r(&rseed_)/RAND_MAX;
+//        #endif
+//        return (x * x + y * y) <= 1.0f;
+//    }
+//};
+
 
 int main(int argc, char* argv[]) {
     struct timeval t1, t2;
@@ -47,26 +82,31 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "Trials: " << N << std::endl;
 
-    srand(time(NULL));
+    unsigned int rseed = time(NULL);
+    //unsigned int rseed =
+    //    static_cast<uint64_t>(std::chrono::system_clock::to_time_t(
+    //        std::chrono::system_clock::now()));
 
-    // generate pi trial
-    auto genpit = [=] __host__ __device__ (UINT64 thread_id) {
+    // generate pi trial lambda function.
+    auto genpit = [=] __host__ __device__ (UINT64 id_or_seed) {
         float x, y;
-        #ifdef __CUDA_ARCH__ // runtime macro
+        #ifdef __CUDA_ARCH__ // macro true on device and false on host.
             thrust::default_random_engine rng(clock());
             thrust::uniform_real_distribution<float> dist(0.0f, 1.0f);
-            rng.discard(thread_id);
+            rng.discard(id_or_seed);
             x = dist(rng);
             y = dist(rng);
         #else
-            x = (float)rand()/RAND_MAX;
-            y = (float)rand()/RAND_MAX;
+            unsigned int rseed_ = id_or_seed;
+            x = (float)rand_r(&rseed_)/RAND_MAX;
+            y = (float)rand_r(&rseed_)/RAND_MAX;
         #endif
         return (x * x + y * y) <= 1.0f;
     };
 
     std::cout << "calculate pi \n";
 
+    // GPU parallelization ####################################################
     gettimeofday(&t1, 0);
     float pi_count = thrust::transform_reduce(thrust::cuda::par,
         thrust::counting_iterator<UINT64>(0),
@@ -79,10 +119,11 @@ int main(int argc, char* argv[]) {
     float pi = pi_count * 4.f / (float) N;
     print_msg("GPU", pi);
 
+    // CPP no parallelization #################################################
     gettimeofday(&t1, 0);
     pi_count = thrust::transform_reduce(thrust::cpp::par,
-        thrust::counting_iterator<UINT64>(0),
-        thrust::counting_iterator<UINT64>(N),
+        thrust::counting_iterator<UINT64>(rseed),
+        thrust::counting_iterator<UINT64>(rseed + N),
         genpit,
         0.0f, thrust::plus<float>());
     gettimeofday(&t2, 0);
@@ -90,32 +131,39 @@ int main(int argc, char* argv[]) {
     pi = pi_count * 4.f / (float) N;
     print_msg("CPP", pi);
 
-    //unsigned int total_threads = std::thread::hardware_concurrency();
-    //std::cout << "\nTotal threads " << total_threads << std::endl;
-
-    // The thrust::omp::par profile very slow. Implementing it manually.
-    int nthreads;
-    pi_count=0;
+    // OMP parallelization ####################################################
     gettimeofday(&t1, 0);
-    #pragma omp parallel reduction(+:pi_count)
-    {
-        nthreads = omp_get_num_threads();
-        // int chunk = N / (UINT64) nthreads + 1; //100000;
-        // UNUSED(chunk); // inhibit warning
+    pi_count = thrust::transform_reduce(thrust::omp::par,
+        thrust::counting_iterator<UINT64>(rseed),
+        thrust::counting_iterator<UINT64>(rseed  + N),
+        genpit,
+        0.0f, thrust::plus<float>());
+    gettimeofday(&t2, 0);
+    pi = pi_count * 4.f / (float) N;
+    print_msg("THRUST OMP", pi);
 
-        //#pragma omp for schedule(dynamic,chunk)
-        #pragma omp for schedule(static)
-        for (UINT64 i = 0; i < N; i++) {
-            pi_count += genpit(i);
-        }
+    // Manual OMP parallelization #############################################
+    // The thrust::omp::par profile seems slow.
+    pi_count = 0;
+    gettimeofday(&t1, 0);
+    #pragma omp parallel for reduction(+:pi_count)
+    for (UINT64 i = 0; i < N; i++) {
+        pi_count += genpit(rseed + i);
     }
     gettimeofday(&t2, 0);
     pi = pi_count * 4.f / (float) N;
+    print_msg("MANUAL OMP", pi);
 
-    std::stringstream msg;
-    msg.str(std::string());
-    msg << "OMP using " << nthreads << " threads.";
-    print_msg(msg.str(), pi);
+    // ########################################################################
+    //unsigned int total_threads = std::thread::hardware_concurrency();
+    //std::cout << "\nTotal threads " << total_threads << std::endl;
+
+    //std::stringstream msg;
+    //msg.str(std::string());
+    //msg << "OMP using " << nthreads << " threads.";
+    //print_msg(msg.str(), pi);
+    int nthreads = omp_get_max_threads();  // use OMP_NUM_THREADS to vary.
+    std::cout << "\nOMP using " << nthreads << " threads." << std::endl;
 
     return 0;
 }
